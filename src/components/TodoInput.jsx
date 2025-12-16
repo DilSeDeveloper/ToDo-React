5adec178a6668c67642c418e0d5825f0eaa7673b
DilSeDeveloper <saumyajain2012004@gmail.com>
2025-12-16 18:40:58 +0530

    validation check

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -6,11 +6,31 @@ function TodoInput({ addList }) {
   const [inputText, setInputText] = useState("");
 
   const handleChange = (e) => {
-    const value = e.target.value;
-    if (/^[a-zA-Z0-9 ]*$/.test(value)) {
-      setInputText(value);
+  const value = e.target.value;
+
+  let hasLetter = false;
+  let hasOnlyValidChars = true;
+
+  for (let char of value) {
+    // check if character is a letter
+    if ((char >= "a" && char <= "z") || (char >= "A" && char <= "Z")) {
+      hasLetter = true;
     }
-  };
+  
+    else if (
+      !((char >= "0" && char <= "9") || char === " ")
+    ) {
+      hasOnlyValidChars = false;
+      break;
+    }
+  }
+
+  if (!hasOnlyValidChars) return;
+  if (!hasLetter && value.trim() !== "") return;
+  setInputText(value);
+};
+
+
 
  const handleAdd = () => {
   if (inputText.trim() === "") return;
